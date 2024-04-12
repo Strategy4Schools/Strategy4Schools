5996fb7385c9f902abfd7b85cfbf6dddcc681a94
Jeong <163141432+Strategy4Schools@users.noreply.github.com>
2024-04-12 08:41:45 +0100

    Update Flashcards.js

--- Flashcards.js
+++ Flashcards.js
@@ -11,20 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let wordListURL;
     switch (flashcardSet) {
         case 'tricky-spellers':
-            wordListURL = 'http://localhost:8000/Year%207-8%20tricky%20spellers%20flashcards.json';
+            wordListURL = 'Year%207-8%20tricky%20spellers%20flashcards.json';
             break;
         case 'useful-verbs':
-            wordListURL = 'http://localhost:8000/Year%207-8%20useful%20verbs%20flashcards.json';
+            wordListURL = 'Year%207-8%20useful%20verbs%20flashcards.json';
             break;
         case 'impressive-adjectives':
-            wordListURL = 'http://localhost:8000/Year%207-8%20useful%20adjectives%20flashcards.json';
+            wordListURL = 'Year%207-8%20useful%20adjectives%20flashcards.json';
             break;
         case 'common-shakespearean-terms':
-            wordListURL = 'http://localhost:8000/Common_Shakespeare_Terms.json';
+            wordListURL = 'Common_Shakespeare_Terms.json';
             break;
         default:
             console.error('Flashcard set not found. Loading default set.');
-            wordListURL = 'http://localhost:8000/default_set.json'; // Fallback word list
+            wordListURL = 'default_set.json'; // Fallback word list
     }
 
     // Fetch the selected word list
